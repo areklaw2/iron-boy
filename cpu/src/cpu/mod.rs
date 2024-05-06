54dff04d6d1079e41e6afe127208d423b66c681a
Aza Walker <azawalker.jr@gmail.com>
2024-05-06 00:50:02 -0400

    finished opcodes

--- cpu/src/cpu/mod.rs
+++ cpu/src/cpu/mod.rs
@@ -104,8 +104,7 @@ impl Cpu {
         self.mmu.mem_write(0xFF0F, interrupt);
 
         let program_counter = self.registers.pc;
-        self.registers.sp = self.registers.sp.wrapping_sub(2);
-        self.mmu.mem_write_16(self.registers.sp, program_counter);
+        self.push_stack(program_counter);
         self.registers.pc = 0x0040 | ((bits as u16) << 3);
 
         16
@@ -113,9 +112,9 @@ impl Cpu {
 
     pub fn run_cycle(&mut self) -> u8 {
         self.update_ime_state();
-        let interrupt = self.handle_interrupt();
-        if interrupt == 0 {
-            return interrupt;
+        match self.handle_interrupt() {
+            0 => {}
+            ticks => return ticks,
         }
 
         if self.halted {
@@ -644,7 +643,314 @@ impl Cpu {
                 self.mmu.mem_write(self.registers.hl(), data);
             }
             0x1F => self.registers.a = self.rr(self.registers.a),
-            code => panic!("Code {:#04X} not implemented", code),
+            0x20 => self.registers.b = self.sla(self.registers.b),
+            0x21 => self.registers.c = self.sla(self.registers.c),
+            0x22 => self.registers.d = self.sla(self.registers.d),
+            0x23 => self.registers.e = self.sla(self.registers.e),
+            0x24 => self.registers.h = self.sla(self.registers.h),
+            0x25 => self.registers.l = self.sla(self.registers.l),
+            0x26 => {
+                let data = self.sla(self.mmu.mem_read(self.registers.hl()));
+                self.mmu.mem_write(self.registers.hl(), data);
+            }
+            0x27 => self.registers.a = self.sla(self.registers.a),
+            0x28 => self.registers.b = self.sra(self.registers.b),
+            0x29 => self.registers.c = self.sra(self.registers.c),
+            0x2A => self.registers.d = self.sra(self.registers.d),
+            0x2B => self.registers.e = self.sra(self.registers.e),
+            0x2C => self.registers.h = self.sra(self.registers.h),
+            0x2D => self.registers.l = self.sra(self.registers.l),
+            0x2E => {
+                let data = self.sra(self.mmu.mem_read(self.registers.hl()));
+                self.mmu.mem_write(self.registers.hl(), data);
+            }
+            0x2F => self.registers.a = self.sra(self.registers.a),
+            0x30 => self.registers.b = self.swap(self.registers.b),
+            0x31 => self.registers.c = self.swap(self.registers.c),
+            0x32 => self.registers.d = self.swap(self.registers.d),
+            0x33 => self.registers.e = self.swap(self.registers.e),
+            0x34 => self.registers.h = self.swap(self.registers.h),
+            0x35 => self.registers.l = self.swap(self.registers.l),
+            0x36 => {
+                let data = self.swap(self.mmu.mem_read(self.registers.hl()));
+                self.mmu.mem_write(self.registers.hl(), data);
+            }
+            0x37 => self.registers.a = self.swap(self.registers.a),
+            0x38 => self.registers.b = self.srl(self.registers.b),
+            0x39 => self.registers.c = self.srl(self.registers.c),
+            0x3A => self.registers.d = self.srl(self.registers.d),
+            0x3B => self.registers.e = self.srl(self.registers.e),
+            0x3C => self.registers.h = self.srl(self.registers.h),
+            0x3D => self.registers.l = self.srl(self.registers.l),
+            0x3E => {
+                let data = self.srl(self.mmu.mem_read(self.registers.hl()));
+                self.mmu.mem_write(self.registers.hl(), data);
+            }
+            0x3F => self.registers.a = self.srl(self.registers.a),
+            0x40 => self.bit(self.registers.b, 0),
+            0x41 => self.bit(self.registers.c, 0),
+            0x42 => self.bit(self.registers.d, 0),
+            0x43 => self.bit(self.registers.e, 0),
+            0x44 => self.bit(self.registers.h, 0),
+            0x45 => self.bit(self.registers.l, 0),
+            0x46 => {
+                let data = self.mmu.mem_read(self.registers.hl());
+                self.bit(data, 0)
+            }
+            0x47 => self.bit(self.registers.a, 0),
+            0x48 => self.bit(self.registers.b, 1),
+            0x49 => self.bit(self.registers.c, 1),
+            0x4A => self.bit(self.registers.d, 1),
+            0x4B => self.bit(self.registers.e, 1),
+            0x4C => self.bit(self.registers.h, 1),
+            0x4D => self.bit(self.registers.l, 1),
+            0x4E => {
+                let data = self.mmu.mem_read(self.registers.hl());
+                self.bit(data, 1)
+            }
+            0x4F => self.bit(self.registers.a, 1),
+            0x50 => self.bit(self.registers.b, 2),
+            0x51 => self.bit(self.registers.c, 2),
+            0x52 => self.bit(self.registers.d, 2),
+            0x53 => self.bit(self.registers.e, 2),
+            0x54 => self.bit(self.registers.h, 2),
+            0x55 => self.bit(self.registers.l, 2),
+            0x56 => {
+                let data = self.mmu.mem_read(self.registers.hl());
+                self.bit(data, 2)
+            }
+            0x57 => self.bit(self.registers.a, 2),
+            0x58 => self.bit(self.registers.b, 3),
+            0x59 => self.bit(self.registers.c, 3),
+            0x5A => self.bit(self.registers.d, 3),
+            0x5B => self.bit(self.registers.e, 3),
+            0x5C => self.bit(self.registers.h, 3),
+            0x5D => self.bit(self.registers.l, 3),
+            0x5E => {
+                let data = self.mmu.mem_read(self.registers.hl());
+                self.bit(data, 3)
+            }
+            0x5F => self.bit(self.registers.a, 3),
+            0x60 => self.bit(self.registers.b, 4),
+            0x61 => self.bit(self.registers.c, 4),
+            0x62 => self.bit(self.registers.d, 4),
+            0x63 => self.bit(self.registers.e, 4),
+            0x64 => self.bit(self.registers.h, 4),
+            0x65 => self.bit(self.registers.l, 4),
+            0x66 => {
+                let data = self.mmu.mem_read(self.registers.hl());
+                self.bit(data, 4)
+            }
+            0x67 => self.bit(self.registers.a, 4),
+            0x68 => self.bit(self.registers.b, 5),
+            0x69 => self.bit(self.registers.c, 5),
+            0x6A => self.bit(self.registers.d, 5),
+            0x6B => self.bit(self.registers.e, 5),
+            0x6C => self.bit(self.registers.h, 5),
+            0x6D => self.bit(self.registers.l, 5),
+            0x6E => {
+                let data = self.mmu.mem_read(self.registers.hl());
+                self.bit(data, 5)
+            }
+            0x6F => self.bit(self.registers.a, 5),
+            0x70 => self.bit(self.registers.b, 6),
+            0x71 => self.bit(self.registers.c, 6),
+            0x72 => self.bit(self.registers.d, 6),
+            0x73 => self.bit(self.registers.e, 6),
+            0x74 => self.bit(self.registers.h, 6),
+            0x75 => self.bit(self.registers.l, 6),
+            0x76 => {
+                let data = self.mmu.mem_read(self.registers.hl());
+                self.bit(data, 6)
+            }
+            0x77 => self.bit(self.registers.a, 6),
+            0x78 => self.bit(self.registers.b, 7),
+            0x79 => self.bit(self.registers.c, 7),
+            0x7A => self.bit(self.registers.d, 7),
+            0x7B => self.bit(self.registers.e, 7),
+            0x7C => self.bit(self.registers.h, 7),
+            0x7D => self.bit(self.registers.l, 7),
+            0x7E => {
+                let data = self.mmu.mem_read(self.registers.hl());
+                self.bit(data, 7)
+            }
+            0x7F => self.bit(self.registers.a, 7),
+            0x80 => self.registers.b = self.registers.b & !(1 << 0),
+            0x81 => self.registers.c = self.registers.c & !(1 << 0),
+            0x82 => self.registers.d = self.registers.d & !(1 << 0),
+            0x83 => self.registers.e = self.registers.e & !(1 << 0),
+            0x84 => self.registers.h = self.registers.h & !(1 << 0),
+            0x85 => self.registers.l = self.registers.l & !(1 << 0),
+            0x86 => {
+                let data = self.mmu.mem_read(self.registers.hl()) & !(1 << 0);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0x87 => self.registers.a = self.registers.a & !(1 << 0),
+            0x88 => self.registers.b = self.registers.b & !(1 << 1),
+            0x89 => self.registers.c = self.registers.c & !(1 << 1),
+            0x8A => self.registers.d = self.registers.d & !(1 << 1),
+            0x8B => self.registers.e = self.registers.e & !(1 << 1),
+            0x8C => self.registers.h = self.registers.h & !(1 << 1),
+            0x8D => self.registers.l = self.registers.l & !(1 << 1),
+            0x8E => {
+                let data = self.mmu.mem_read(self.registers.hl()) & !(1 << 1);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0x8F => self.registers.a = self.registers.a & !(1 << 1),
+            0x90 => self.registers.b = self.registers.b & !(1 << 2),
+            0x91 => self.registers.c = self.registers.c & !(1 << 2),
+            0x92 => self.registers.d = self.registers.d & !(1 << 2),
+            0x93 => self.registers.e = self.registers.e & !(1 << 2),
+            0x94 => self.registers.h = self.registers.h & !(1 << 2),
+            0x95 => self.registers.l = self.registers.l & !(1 << 2),
+            0x96 => {
+                let data = self.mmu.mem_read(self.registers.hl()) & !(1 << 2);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0x97 => self.registers.a = self.registers.a & !(1 << 2),
+            0x98 => self.registers.b = self.registers.b & !(1 << 3),
+            0x99 => self.registers.c = self.registers.c & !(1 << 3),
+            0x9A => self.registers.d = self.registers.d & !(1 << 3),
+            0x9B => self.registers.e = self.registers.e & !(1 << 3),
+            0x9C => self.registers.h = self.registers.h & !(1 << 3),
+            0x9D => self.registers.l = self.registers.l & !(1 << 3),
+            0x9E => {
+                let data = self.mmu.mem_read(self.registers.hl()) & !(1 << 3);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0x9F => self.registers.a = self.registers.a & !(1 << 3),
+            0xA0 => self.registers.b = self.registers.b & !(1 << 4),
+            0xA1 => self.registers.c = self.registers.c & !(1 << 4),
+            0xA2 => self.registers.d = self.registers.d & !(1 << 4),
+            0xA3 => self.registers.e = self.registers.e & !(1 << 4),
+            0xA4 => self.registers.h = self.registers.h & !(1 << 4),
+            0xA5 => self.registers.l = self.registers.l & !(1 << 4),
+            0xA6 => {
+                let data = self.mmu.mem_read(self.registers.hl()) & !(1 << 4);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xA7 => self.registers.a = self.registers.a & !(1 << 4),
+            0xA8 => self.registers.b = self.registers.b & !(1 << 5),
+            0xA9 => self.registers.c = self.registers.c & !(1 << 5),
+            0xAA => self.registers.d = self.registers.d & !(1 << 5),
+            0xAB => self.registers.e = self.registers.e & !(1 << 5),
+            0xAC => self.registers.h = self.registers.h & !(1 << 5),
+            0xAD => self.registers.l = self.registers.l & !(1 << 5),
+            0xAE => {
+                let data = self.mmu.mem_read(self.registers.hl()) & !(1 << 5);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xAF => self.registers.a = self.registers.a & !(1 << 5),
+            0xB0 => self.registers.b = self.registers.b & !(1 << 6),
+            0xB1 => self.registers.c = self.registers.c & !(1 << 6),
+            0xB2 => self.registers.d = self.registers.d & !(1 << 6),
+            0xB3 => self.registers.e = self.registers.e & !(1 << 6),
+            0xB4 => self.registers.h = self.registers.h & !(1 << 6),
+            0xB5 => self.registers.l = self.registers.l & !(1 << 6),
+            0xB6 => {
+                let data = self.mmu.mem_read(self.registers.hl()) & !(1 << 6);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xB7 => self.registers.a = self.registers.a & !(1 << 6),
+            0xB8 => self.registers.b = self.registers.b & !(1 << 7),
+            0xB9 => self.registers.c = self.registers.c & !(1 << 7),
+            0xBA => self.registers.d = self.registers.d & !(1 << 7),
+            0xBB => self.registers.e = self.registers.e & !(1 << 7),
+            0xBC => self.registers.h = self.registers.h & !(1 << 7),
+            0xBD => self.registers.l = self.registers.l & !(1 << 7),
+            0xBE => {
+                let data = self.mmu.mem_read(self.registers.hl()) & !(1 << 7);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xBF => self.registers.a = self.registers.a & !(1 << 7),
+            0xC0 => self.registers.b = self.registers.b | (1 << 0),
+            0xC1 => self.registers.c = self.registers.c | (1 << 0),
+            0xC2 => self.registers.d = self.registers.d | (1 << 0),
+            0xC3 => self.registers.e = self.registers.e | (1 << 0),
+            0xC4 => self.registers.h = self.registers.h | (1 << 0),
+            0xC5 => self.registers.l = self.registers.l | (1 << 0),
+            0xC6 => {
+                let data = self.mmu.mem_read(self.registers.hl()) | (1 << 0);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xC7 => self.registers.a = self.registers.a | (1 << 0),
+            0xC8 => self.registers.b = self.registers.b | (1 << 1),
+            0xC9 => self.registers.c = self.registers.c | (1 << 1),
+            0xCA => self.registers.d = self.registers.d | (1 << 1),
+            0xCB => self.registers.e = self.registers.e | (1 << 1),
+            0xCC => self.registers.h = self.registers.h | (1 << 1),
+            0xCD => self.registers.l = self.registers.l | (1 << 1),
+            0xCE => {
+                let data = self.mmu.mem_read(self.registers.hl()) | (1 << 1);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xCF => self.registers.a = self.registers.a | (1 << 1),
+            0xD0 => self.registers.b = self.registers.b | (1 << 2),
+            0xD1 => self.registers.c = self.registers.c | (1 << 2),
+            0xD2 => self.registers.d = self.registers.d | (1 << 2),
+            0xD3 => self.registers.e = self.registers.e | (1 << 2),
+            0xD4 => self.registers.h = self.registers.h | (1 << 2),
+            0xD5 => self.registers.l = self.registers.l | (1 << 2),
+            0xD6 => {
+                let data = self.mmu.mem_read(self.registers.hl()) | (1 << 2);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xD7 => self.registers.a = self.registers.a | (1 << 2),
+            0xD8 => self.registers.b = self.registers.b | (1 << 3),
+            0xD9 => self.registers.c = self.registers.c | (1 << 3),
+            0xDA => self.registers.d = self.registers.d | (1 << 3),
+            0xDB => self.registers.e = self.registers.e | (1 << 3),
+            0xDC => self.registers.h = self.registers.h | (1 << 3),
+            0xDD => self.registers.l = self.registers.l | (1 << 3),
+            0xDE => {
+                let data = self.mmu.mem_read(self.registers.hl()) | (1 << 3);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xDF => self.registers.a = self.registers.a | (1 << 3),
+            0xE0 => self.registers.b = self.registers.b | (1 << 4),
+            0xE1 => self.registers.c = self.registers.c | (1 << 4),
+            0xE2 => self.registers.d = self.registers.d | (1 << 4),
+            0xE3 => self.registers.e = self.registers.e | (1 << 4),
+            0xE4 => self.registers.h = self.registers.h | (1 << 4),
+            0xE5 => self.registers.l = self.registers.l | (1 << 4),
+            0xE6 => {
+                let data = self.mmu.mem_read(self.registers.hl()) | (1 << 4);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xE7 => self.registers.a = self.registers.a | (1 << 4),
+            0xE8 => self.registers.b = self.registers.b | (1 << 5),
+            0xE9 => self.registers.c = self.registers.c | (1 << 5),
+            0xEA => self.registers.d = self.registers.d | (1 << 5),
+            0xEB => self.registers.e = self.registers.e | (1 << 5),
+            0xEC => self.registers.h = self.registers.h | (1 << 5),
+            0xED => self.registers.l = self.registers.l | (1 << 5),
+            0xEE => {
+                let data = self.mmu.mem_read(self.registers.hl()) | (1 << 5);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xEF => self.registers.a = self.registers.a | (1 << 5),
+            0xF0 => self.registers.b = self.registers.b | (1 << 6),
+            0xF1 => self.registers.c = self.registers.c | (1 << 6),
+            0xF2 => self.registers.d = self.registers.d | (1 << 6),
+            0xF3 => self.registers.e = self.registers.e | (1 << 6),
+            0xF4 => self.registers.h = self.registers.h | (1 << 6),
+            0xF5 => self.registers.l = self.registers.l | (1 << 6),
+            0xF6 => {
+                let data = self.mmu.mem_read(self.registers.hl()) | (1 << 6);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xF7 => self.registers.a = self.registers.a | (1 << 6),
+            0xF8 => self.registers.b = self.registers.b | (1 << 7),
+            0xF9 => self.registers.c = self.registers.c | (1 << 7),
+            0xFA => self.registers.d = self.registers.d | (1 << 7),
+            0xFB => self.registers.e = self.registers.e | (1 << 7),
+            0xFC => self.registers.h = self.registers.h | (1 << 7),
+            0xFD => self.registers.l = self.registers.l | (1 << 7),
+            0xFE => {
+                let data = self.mmu.mem_read(self.registers.hl()) | (1 << 7);
+                self.mmu.mem_write(self.registers.hl(), data)
+            }
+            0xFF => self.registers.a = self.registers.a | (1 << 7),
         }
         (mnemonic, tcycles)
     }
@@ -820,6 +1126,42 @@ impl Cpu {
         let result = (data >> 1) | (if self.registers.f.contains(CpuFlag::C) { 0x80 } else { 0 });
         self.rotate_shift_flag_update(result, carry);
         result
+    }
+
+    fn sla(&mut self, data: u8) -> u8 {
+        let carry = data & 0x80 == 0x80;
+        let result = data << 1;
+        self.rotate_shift_flag_update(result, carry);
+        result
+    }
+
+    fn sra(&mut self, data: u8) -> u8 {
+        let carry = data & 0x01 == 0x01;
+        let result = (data >> 1) | (data & 0x80);
+        self.rotate_shift_flag_update(result, carry);
+        result
+    }
+
+    fn swap(&mut self, data: u8) -> u8 {
+        self.registers.set_flag(CpuFlag::Z, data == 0);
+        self.registers.set_flag(CpuFlag::N, false);
+        self.registers.set_flag(CpuFlag::H, false);
+        self.registers.set_flag(CpuFlag::C, false);
+        (data >> 4) | (data << 4)
+    }
+
+    fn srl(&mut self, data: u8) -> u8 {
+        let carry = data & 0x01 == 0x01;
+        let result = data >> 1;
+        self.rotate_shift_flag_update(result, carry);
+        result
+    }
+
+    fn bit(&mut self, data: u8, bit: u8) {
+        let result = data & (1 << (bit as u32)) == 0;
+        self.registers.set_flag(CpuFlag::Z, result);
+        self.registers.set_flag(CpuFlag::N, false);
+        self.registers.set_flag(CpuFlag::H, false);
     }
 
     // branch
