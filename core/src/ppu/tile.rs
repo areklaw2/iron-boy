769e6679368f5494361ea5388341c20570829d97
Aza Walker <azawalker.jr@gmail.com>
2024-06-08 22:38:29 -0400

    using tiledata and some variable clean and refactoring

--- core/src/ppu/tile.rs
+++ core/src/ppu/tile.rs
@@ -9,12 +9,3 @@ pub enum TileData {
     Block0 = 0x8000,
     Block1 = 0x8800,
 }
-
-impl TileData {
-    pub fn address(self, tile: u8) -> u16 {
-        match self {
-            TileData::Block0 => (0x1000 + (((tile as i8) as i16) * 16)) as u16,
-            TileData::Block1 => 0x0 + (tile as u16) * 16,
-        }
-    }
-}
