c6452717b19c92e3a95cd4c4e9e30c2268f564f4
Aza Walker <azawalker.jr@gmail.com>
2024-12-22 20:02:01 -0500

    adding ticks to log

--- core/src/cpu/mod.rs
+++ core/src/cpu/mod.rs
@@ -226,7 +226,8 @@ impl<I: MemoryInterface> Cpu<I> {
         );
 
         let log = format!(
-            "{:#06X}: {:<20} ({:#04X} {:#04X} {:#04X}) A: {:#04X} F: {flags} BC: {:#06X} DE: {:#06X} HL: {:#06X} SP: {:#06X}\n",
+            "{}: {:#06X}: {:<20} ({:#04X} {:#04X} {:#04X}) A: {:#04X} F: {flags} BC: {:#06X} DE: {:#06X} HL: {:#06X} SP: {:#06X}\n",
+            self.total_cycles,
             pc,
             &self
                 .current_instruction
