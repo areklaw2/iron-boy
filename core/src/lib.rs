6221f4c5ed9c58d96a9f2ea68d3fcb4a90563c87
Aza Walker <azawalker.jr@gmail.com>
2024-12-25 02:47:32 -0500

    README updates

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -10,10 +10,6 @@ pub use crate::apu::{AUDIO_BUFFER_THRESHOLD, SAMPLING_FREQUENCY, SAMPLING_RATE};
 pub use crate::io::joypad::JoypadButton;
 pub use crate::ppu::{FPS, VIEWPORT_HEIGHT, VIEWPORT_WIDTH};
 
-pub trait Component {
-    fn cycle(&mut self, cycles: u32);
-}
-
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum GameBoyMode {
     Monochrome,
